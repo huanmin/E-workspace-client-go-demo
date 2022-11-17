1745ec9584ac64f79d2a032505d498e6909d53d5
huanmin <1160761653@qq.com>
2022-11-17 11:05:20 +0800

    kubebuilder-demo controller

--- kubebuilder-demo/controllers/app_controller.go
+++ kubebuilder-demo/controllers/app_controller.go
@@ -18,6 +18,14 @@ package controllers
 
 import (
 	"context"
+	"github.ocm/kubebuilder-demo/controllers/utils"
+	v1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	netv1 "k8s.io/api/networking/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
+	log2 "log"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -49,7 +57,80 @@ type AppReconciler struct {
 func (r *AppReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	app := &examplecomv1beta1.App{}
+
+	if err := r.Get(ctx, req.NamespacedName, app); err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	deployment := utils.NewDeployment(app)
+	if err := controllerutil.SetControllerReference(app, deployment, r.Scheme); err != nil {
+		return ctrl.Result{}, err
+	}
+	d := &v1.Deployment{}
+	if err := r.Get(ctx, req.NamespacedName, d); err != nil {
+		if errors.IsNotFound(err) {
+			if err := r.Create(ctx, deployment); err != nil {
+				log2.Fatalln(err, "create deploy failed")
+				return ctrl.Result{}, err
+			}
+		}
+	} else {
+		if err := r.Update(ctx, deployment); err != nil {
+			return ctrl.Result{}, err
+		}
+	}
+
+	service := utils.NewService(app)
+	if err := controllerutil.SetControllerReference(app, service, r.Scheme); err != nil {
+		return ctrl.Result{}, err
+	}
+	s := &corev1.Service{}
+	if err := r.Get(ctx, types.NamespacedName{Namespace: app.Namespace, Name: app.Name}, s); err != nil {
+		if errors.IsNotFound(err) {
+			if err := r.Create(ctx, service); err != nil {
+				log2.Fatalln(err, "create service failed")
+				return ctrl.Result{}, err
+			}
+		}
+	} else {
+		if app.Spec.EnableService {
+			if err := r.Update(ctx, service); err != nil {
+				return ctrl.Result{}, err
+			} else {
+				if err := r.Delete(ctx, s); err != nil {
+					return ctrl.Result{}, err
+				}
+			}
+		}
+	}
+
+	if !app.Spec.EnableService {
+		return ctrl.Result{}, nil
+	}
+	ingress := utils.NewIngress(app)
+	if err := controllerutil.SetControllerReference(app, ingress, r.Scheme); err != nil {
+		return ctrl.Result{}, err
+	}
+	i := &netv1.Ingress{}
+	if err := r.Get(ctx, types.NamespacedName{Namespace: app.Namespace, Name: app.Name}, i); err != nil {
+		if errors.IsNotFound(err) {
+			if err := r.Create(ctx, ingress); err != nil {
+				log2.Fatalln(err, "create ingress failed")
+				return ctrl.Result{}, err
+			}
+		}
+	} else {
+		if app.Spec.EnableIngress {
+			if err := r.Update(ctx, ingress); err != nil {
+				return ctrl.Result{}, err
+			} else {
+				if err := r.Delete(ctx, i); err != nil {
+					return ctrl.Result{}, err
+				}
+			}
+		}
+	}
 
 	return ctrl.Result{}, nil
 }
@@ -58,5 +139,8 @@ func (r *AppReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.R
 func (r *AppReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&examplecomv1beta1.App{}).
+		Owns(&v1.Deployment{}).
+		Owns(&netv1.Ingress{}).
+		Owns(&corev1.Service{}).
 		Complete(r)
 }
